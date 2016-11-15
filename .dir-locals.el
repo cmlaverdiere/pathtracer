5af2cc3813576d06a3bdd823ac27445c21292e90
Chris Laverdiere <cmlaverdiere@gmail.com>
2016-11-14 20:14:21 -0500

    Fix dir_locals clang

--- .dir-locals.el
+++ .dir-locals.el
@@ -3,4 +3,5 @@
 
 ((c++-mode
   (flycheck-clang-language-standard . "c++11")
+  (company-clang-arguments . ("-std=c++11" "-I./lib" "-L./lib"))
   (flycheck-clang-include-path "./lib" "/usr/include/c++/6.2.1/")))
